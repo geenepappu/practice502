34058a428e48b6789f0301a69511c45198d4771f
Gowthami <GowthamiB@hexaware.com>
2019-02-05 11:37:26 +0530

    The first Push

---
+++ practice1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class practice1
+{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		add(a,b);
+		sub(a,b);
+		mul(a,b);
+		div(a,b);
+		
+	}
+	public void add()
+	{
+		//zakir
+	}
+
+	public void sub()
+	{
+		//bhaskar
+	}
+
+	public void mul()
+	{
+		//swetha
+
+	}
+
+	public void div()
+	{
+		//juana
+	}
+
+
+}

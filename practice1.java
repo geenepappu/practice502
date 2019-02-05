502225863686414b4a43b6a3debbed2ad3c21259
JuanaV-Hexaware <JuanaV@hexaware.com>
2019-02-05 12:22:01 +0530

    Juana commit

--- practice1.java
+++ practice1.java
@@ -1,41 +1,37 @@
 import java.util.*;
 
-class practice1
-{
-	public static void main(String args[])
-	{
-		int a,b,c;
-		Scanner sc=new Scanner(System.in);
-		a=sc.nextInt();
-		b=sc.nextInt();
-		add(a,b);
-		sub(a,b);
-		mul(a,b);
-		div(a,b);
-		
-	}
-	public void add()
-	{
-		//zakir
+class practice1 {
+	public static void main(String args[]) {
+		int a, b, c;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		add(a, b);
+		sub(a, b);
+		mul(a, b);
+		div(a, b);
+
 	}
 
-	public void sub()
-	{
-		//bhaskar
+	public void add() {
+		// zakir
 	}
 
-	public void mul()
-	{
-		c=a*b;
+	public void sub() {
+		// bhaskar
+	}
+
+	public void mul() {
+		// Swetha
+		c = a * b;
 		System.out.println(c);
 
-
 	}
 
-	public void div()
-	{
-		//juana
+	public void div() {
+		// Juana
+		c = a / b;
+		System.out.println(c);
 	}
-
 
 }
